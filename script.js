0e15d69df2bb67d0c27cf1e84161041fe8562f64
reltaher <43248170+reltaher@users.noreply.github.com>
2020-10-22 13:57:00 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 $(document).ready(function() {
-    $("#navigation li a").click(function(e) {
-    e.preventDefault();
-
-    var targetElement = $(this).attr("href");
-    var targetPosition = $(targetElement).offset().top;
-    $("html, body").animate({scrollTop: targetPosition - 50}, "slow");
-});
-
-const nav = $("#navigation");
-const navTop = nav.offset().top;
-
-$(window).on("scroll", stickyNavigation);
 
 function stickyNavigation() {
 
